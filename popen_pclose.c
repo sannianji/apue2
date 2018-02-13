6022707b394c652de4868702cf8626761480fcc0
sansansan <sannianjisansan@gmail.com>
2018-02-13 18:13:48 +0800

    unit 15

--- popen_pclose.c
+++ popen_pclose.c
@@ -84,3 +84,38 @@ FILE* popen(const char *cmdstring,const char *type)
 	childpid[fileno(fp)]=pid;
 	return (fp);
 }
+
+int pclose(FILE* fp)
+{
+	int fd,stat;
+	pid_t pid;
+	
+	if(childpd==NULL)
+	{
+		errno=EINVAL;
+		return (-1);
+	}
+
+	fd=fileno(fp);
+	if(fd>=maxfd)
+	{
+		errno=EINVAL;
+		return (-1);
+	}
+	if((pid=childpid[fd])==0)
+	{
+		errno=EINVAL;
+		reutn (-1);
+	}
+
+	childpid[fd]=0;
+	if(fclose(fp)==EOF)
+	{
+		return (-1);
+	}
+
+	while(waitpid(pid,&status,0)<0)
+		if(errno!=EINTR)
+			return (-1);
+	return stat;
+}
