37f00926d3fdba3d6f88ffad2a34e7730db3eb08
sansansan <sannianjisansan@gmail.com>
2018-05-03 14:18:16 +0800

    opend

--- buf_args.c
+++ buf_args.c
@@ -2,4 +2,19 @@
 #define MAXARGC 50
 #define WHITE " \t\n"
 
-int buf_args(char *buf)
+int buf_args(char *buf,int (*optfunc)(int,char **))
+{
+	char *ptr,*argv[MAXARGC];
+	int argc;
+	if(strtok(buf,WHITE)==NULL)
+		return -1;
+	argv[argc=0]=buf;
+	while((ptr=strtok(NULL,WHITE))!=NULL)
+	{
+		if(++argc>=MAXARGC-1)
+			return -1;
+		argv[argc]=ptr;
+	}
+	argv[++argc]=NULL;
+	return ((*optfunc)(argc,argv));
+}
